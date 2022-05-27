d22b689dde37f79e3a6989d0aa5c6aadcd7a2232
Usman Kulaha <32143087+UK818@users.noreply.github.com>
2022-05-27 16:21:04 +0100

    created data model for post fetched from database

--- blinqpost/Model/PostModel.swift
+++ blinqpost/Model/PostModel.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+struct Post: Codable {
+	let welcomeDescription: String?
+	let link: String?
+	let id, video, timestamp: Int?
+	let thumbnail: String?
+	let username: String?
+
+	enum CodingKeys: String, CodingKey {
+		case welcomeDescription = "description"
+		case link, id, video, timestamp, thumbnail, username
+	}
+}
