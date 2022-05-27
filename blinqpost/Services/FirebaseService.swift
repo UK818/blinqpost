daaf3fc887ea92b49213c675cafdad8778cf6042
Usman Kulaha <32143087+UK818@users.noreply.github.com>
2022-05-27 16:20:13 +0100

    created firebase service class and read data from Firestore

--- blinqpost/Services/FirebaseService.swift
+++ blinqpost/Services/FirebaseService.swift
@@ -8,9 +8,26 @@
 import Foundation
 import FirebaseFirestore
 
+
+
 class FirebaseService {
 	
-	let database = Firestore.firestore()
+	private let database = Firestore.firestore()
+	var postCollection: [String: Any] = [:]
 	
+	func fetchPosts() {
+		database.collection("post").getDocuments() { [weak self] (querySnapshot, err) in
+			if let err = err {
+				print("Error getting documents: \(err)")
+			} else {
+				for document in querySnapshot!.documents {
+					let posts = document.data()
+					print(posts)
+					self?.postCollection = document.data()
+//					print("\(document.documentID) => \(document.data().count)")
+				}
+			}
+		}
+	}
 	
 }
